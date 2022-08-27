b67bd02d8996fb97f4ed146854e946f2bc1deb5f
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2022-08-27 18:07:40 +0700

    Ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LayOut from "./components/LayOut/LayOut";
 import Hocvien from "./pages/Hocvien";
 import Home from "./pages/Home";
-import Licthi from "./pages/Licthi";
+import Lichthi from "./pages/Lichthi";
 import TKB from "./pages/TKB";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/hocvien" element={<Hocvien />} />
-          <Route path="/lichthi" element={<Licthi />} />
+          <Route path="/lichthi" element={<Lichthi />} />
           <Route path="/tkb" element={<TKB />} />
         </Routes>
       </LayOut>
