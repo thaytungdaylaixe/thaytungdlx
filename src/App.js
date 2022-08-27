f7b0898c5bd21f2e49663644f9b988ef82e329bf
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2022-08-27 17:50:55 +0700

    Ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Chao Thay Tung</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
