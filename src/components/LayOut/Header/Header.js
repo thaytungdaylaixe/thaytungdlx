636285ffa26232dc01c16a4781c249aacaf6fa0f
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2022-08-27 18:17:06 +0700

    Ok

--- src/components/LayOut/Header/Header.js
+++ src/components/LayOut/Header/Header.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ClearIcon from "@mui/icons-material/Clear";
